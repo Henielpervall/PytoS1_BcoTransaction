5b98db641ba94adc82eb50c4d93fd6125f309125
hectorperez <hperezva@emeal.nttdata.com>
2022-03-11 18:28:13 -0500

    Se agregó estructura inicial, springboot, maven (microserv transacciones).

---
+++ src/main/java/com/bootcamp/PytoS1_BcoTransaction/PytoS1BcoTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.PytoS1_BcoTransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PytoS1BcoTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PytoS1BcoTransactionApplication.class, args);
+	}
+
+}
